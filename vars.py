d1a3d7d1f169aac60019559e65d970e94c19f405
Rohitmeena8923 <157653731+Rohitmeena8923@users.noreply.github.com>
2025-08-24 10:39:56 +0530

    Update vars.py

--- vars.py
+++ vars.py
@@ -13,8 +13,8 @@ DATABASE_URL = os.environ.get("DATABASE_URL", "")
 MONGO_URL = DATABASE_URL  # For auth system
 
 # Owner and Admin Configuration
-OWNER_ID = int(os.environ.get("OWNER_ID", "8458169280"))
-ADMINS = [int(x) for x in os.environ.get("ADMINS", "8458169280").split()]  # Default to owner ID
+OWNER_ID = int(os.environ.get("OWNER_ID", "6414266397"))
+ADMINS = [int(x) for x in os.environ.get("ADMINS", "6414266397").split()]  # Default to owner ID
 
 # Channel Configuration
 PREMIUM_CHANNEL = "https://t.me/+W-Q51EuLf2QwYTl"
